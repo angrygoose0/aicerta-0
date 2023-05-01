f2490ae9b829ab5b575f84720f9cfb73d9bd36be
wonyoung <kirball125@gmail.com>
2023-05-01 15:11:45 +1200

    fixed adming.py - accounts

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
-from plans.models import Plan
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
