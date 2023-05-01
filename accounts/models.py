33644b5a1c697ca7dcc6a4d0a5bbf45337f25f07
wonyoung <kirball125@gmail.com>
2023-05-01 14:42:10 +1200

    fixed the deafuly plan models

--- accounts/models.py
+++ accounts/models.py
@@ -4,8 +4,14 @@ from plans.models import Plan
 
 
 def get_default_plan():
-    default_plan = Plan.objects.get(name="Basic")
-    return default_plan.pk
+    try:
+        default_plan = Plan.objects.get(name="Basic")
+        return default_plan.pk
+    except Plan.DoesNotExist:
+        # Handle the case when the "Basic" plan does not exist, e.g., create it
+        default_plan = Plan.objects.create(name="Basic", type="Individual", monthly_marking=9, custom_monthly=0, pricing_monthly=10, pricing_yearly=7)
+        return default_plan.pk
+
 
 class CustomUser(AbstractUser):
     plan = models.ForeignKey(Plan, on_delete=models.CASCADE, default=get_default_plan, null=True)
