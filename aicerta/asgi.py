3b6aaadfc841f2b19288bc03407162bce15e3aaf
angrygoose0 <131853530+angrygoose0@users.noreply.github.com>
2023-09-20 10:35:38 +1200

    Update asgi.py

--- aicerta/asgi.py
+++ aicerta/asgi.py
@@ -20,12 +20,20 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aicerta.settings")
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
-
+"""
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
         "websocket": AllowedHostsOriginValidator(
             AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
         ),
+    }
+)
+"""
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
     }
 )
