83840871879d9f789226de796def4068085ffac2
angrygoose0 <131853530+angrygoose0@users.noreply.github.com>
2023-10-18 11:07:58 +1300

    'https://syd1.digitaloceanspaces.com'

--- aicerta/settings.py
+++ aicerta/settings.py
@@ -264,7 +264,9 @@ else:
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = 'aicerta-bucket'
-    AWS_S3_ENDPOINT_URL = 'https://aicerta-bucket.syd1.digitaloceanspaces.com'
+    AWS_S3_ENDPOINT_URL = 'https://syd1.digitaloceanspaces.com'
+    AWS_S3_REGION_NAME = 'syd1'
+
     AWS_DEFAULT_ACL = 'private'
     AWS_S3_OBJECT_PARAMETERS = {
         'CacheControl': 'max-age=86400',
