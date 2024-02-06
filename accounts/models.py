f42e9b58f8b4bd4ec8eef299adf8026f3f019dc2
wonyoung <kirball125@gmail.com>
2024-02-06 13:44:01 +1300

    a

--- accounts/models.py
+++ accounts/models.py
@@ -13,7 +13,7 @@ class CustomUser(AbstractUser):
     credits = models.IntegerField(default=0) #cents
     stripe_customer_id = models.CharField(max_length=50, blank=True, null=True)
     
-    student = models.BooleanField(default=False) #true for student accounts #false for teacher accounts
+    student = models.BooleanField(default=True) #true for student accounts #false for teacher accounts
 
     def __str__(self):
         return self.email
