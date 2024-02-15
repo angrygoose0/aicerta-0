35021b937d76e1c8c7c7a2a9c710df9b32e5d889
wonyoung <kirball125@gmail.com>
2024-02-16 08:21:14 +1300

    a

--- aicerta/asgi.py
+++ aicerta/asgi.py
@@ -8,18 +8,19 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-from .routing import websocket_urlpatterns
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "aicerta.settings")
+
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
+# Now that Django is fully initialized, import the rest of the dependencies
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from .routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
@@ -29,4 +30,3 @@ application = ProtocolTypeRouter(
         ),
     }
 )
-
